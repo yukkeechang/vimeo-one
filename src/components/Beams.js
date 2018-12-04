80b81d9ec5aa68fc4697921d1018b1277d072f52
Yukkee chang <yukkeechang@gmail.com>
2018-12-04 14:30:44 -0500

    started the monsoon/beams layout using CSS grid

---
+++ src/components/Beams.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Beams extends Component {
+  render() {
+    return (
+      <div>
+      <div className="Beams">
+
+        <div>
+         <h4 style={{fontSize: '20px'}}>MONSOON III</h4>
+         <p>Lorem ipsum dolor amet wolf cronut try-hard kombucha, venmo succulents lo-fi cred offal migas cornhole semiotics. Seitan cray ethical austin coloring book live-edge yuccie. Thundercats squid glossier salvia gluten-free kitsch, af shabby chic godard swag poutine street art heirloom intelligentsia brooklyn. Kitsch air plant</p>
+        </div>
+        <img src="https://i.vimeocdn.com/video/589972810_530x315.jpg"/>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Beams;
