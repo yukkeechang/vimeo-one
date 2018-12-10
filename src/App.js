97d27e4eb43c7238981a9ab04d95a5c4d5cda522
Yukkee chang <yukkeechang@gmail.com>
2018-12-10 18:04:22 -0500

    API call attempt and used env variables

--- src/App.js
+++ src/App.js
@@ -6,8 +6,37 @@ import Image from './components/ImageComponent';
 import Title from './components/TitleComponent';
 import Text from './components/TextComponent';
 
-class App extends Component {
+const id = process.env.REACT_APP_CLIENT_ID
+const secret = process.env.REACT_APP_CLIENT_SECRET
+const token = process.env.REACT_APP_ACCESS_TOKEN
 
+let Vimeo = require('vimeo').Vimeo;
+let client = new Vimeo(id, secret, token);
+
+//arrays I would use to store image links from API call
+let images =[];
+let vimeoImages =[];
+
+  client.request({
+    method: 'GET',
+    path: '/categories'
+    //THIS FUNCTION RETURNS ME THE OBJECT BODY..which I need outside of this scope
+  }, function (error, body, status_code, headers) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(body);
+    //querying through data from the API call to find link of images
+    //console.log(body.data[1].pictures.sizes[4].link)
+    images = body.data
+    //issue: I can't seem to get the data from body outside of the API call (set it to a global variable and refer it back inside my App component)
+  })
+  //how I would get an array of image links from the Vimeo API and use it in place of the placeholder images by mapping the components
+  // for (let i =0; i <images.length; i++) {
+  //   vimeoImages.append(i.pictures.sizes[4].link)
+  // }
+
+class App extends Component {
   render() {
     let filler="Lorem ipsum dolor amet wolf cronut try-hard kombucha, venmo succulents lo-fi cred offal migas cornhole semiotics. Seitan cray ethical austin coloring book live-edge yuccie. Thundercats squid glossier salvia gluten-free kitsch, af shabby chic godard swag poutine street art heirloom intelligentsia brooklyn. Kitsch air plant"
     return (
@@ -39,5 +68,4 @@ class App extends Component {
   }
 }
 
-//amet wolf cronut try-hard kombucha, venmo succulents lo-fi cred offal migas cornhole semiotics. Seitan cray ethical austin coloring book live-edge yuccie. Thundercats squid glossier salvia gluten-free kitsch, af shabby chic godard swag poutine street art heirloom intelligentsia brooklyn. Kitsch air plant
 export default App;
